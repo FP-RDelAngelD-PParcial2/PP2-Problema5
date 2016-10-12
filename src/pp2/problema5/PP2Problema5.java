e42886ac810cf68eb0580b5188c4342517abacb4
PC <PC@HP>
2016-10-12 13:23:18 -0500

    5.	Realizar un programa capaz de recibir un número y determinar si este es primo no.

---
+++ src/pp2/problema5/PP2Problema5.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pp2.problema5;
+import java.util.*;
+/**
+ *
+ * @author PC
+ */
+public class PP2Problema5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    public static int solicitarDatos(String d){ //Solicita datos
+        int miDato;
+        Scanner entradaEscaner = new Scanner(System.in);
+        System.out.print("Introduce el dato " + d);
+        miDato= entradaEscaner.nextInt();
+        return miDato;
+    }
+}
